8691c70f162cfc119ceb5d0ffdeec7aeb6da76b0
eren.ozturk <erenozturk@gmail.com>
2021-02-23 19:51:00 +0300

    date picker deiştirildi

--- src/ui/Form/FormDatePicker.tsx
+++ src/ui/Form/FormDatePicker.tsx
@@ -1,10 +1,11 @@
-import React, { FC } from 'react';
-import { Icon, Item, Text } from 'native-base';
+import React, { FC, useState } from 'react';
+import { Button, Icon, Item, Text } from 'native-base';
 import { Controller, useFormContext } from 'react-hook-form';
-import DatePicker from 'react-native-datepicker';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import { FormDatePickerProps } from './FormTypes';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
+import { Platform, View } from 'react-native';
 
 const ErrorText = styled(Text)`
   color: #ff0033;
@@ -14,6 +15,8 @@ export const FormDatePicker: FC<FormDatePickerProps> = ({ name, defaultValue = '
   const methods = useFormContext();
   const { t } = useTranslation();
 
+  const [show, setShow] = useState<boolean>(false);
+
   return (
     <Controller
       name={name}
@@ -21,21 +24,27 @@ export const FormDatePicker: FC<FormDatePickerProps> = ({ name, defaultValue = '
       defaultValue={defaultValue}
       render={({ value, onChange }) => (
         <>
-          <Item style={{ marginTop: 20, marginLeft: 0 }} error={!!methods.errors[name]}>
-            <DatePicker
-              style={{ width: 40 }}
-              date={value}
-              mode='date'
-              format='DD/MM/YYYY'
-              confirmBtnText={t('confirm')}
-              cancelBtnText={t('cancel')}
-              onDateChange={(t, date) => {
+          <View style={{ position: 'absolute', top: 100 }}>
+            <DateTimePickerModal
+              onCancel={() => setShow(false)}
+              onConfirm={(date) => {
                 onChange(date);
+                setShow(Platform.OS === 'ios');
+                setShow(false);
               }}
-              hideText
-              iconComponent={<Icon name='calendar' type='AntDesign' />}
-              {...rest}
+              mode='date'
+              is24Hour
+              display='spinner'
+              isVisible={show}
+              confirmTextIOS={t('confirm')}
+              cancelTextIOS={t('cancel')}
+              headerTextIOS={t('select')}
             />
+          </View>
+          <Item style={{ marginTop: 20, marginLeft: 0 }} error={!!methods.errors[name]}>
+            <Button transparent onPress={() => setShow(true)}>
+              <Icon name='calendar' type='AntDesign' />
+            </Button>
             <Text style={{ color: 'grey' }}>{!value ? t(placeholderTx) : value.toLocaleDateString()}</Text>
             {!!methods.errors[name] && <Icon name='close-circle' />}
           </Item>
